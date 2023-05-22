cf0489b15d4cb37b77529050c88a347705980d7c
Anya <anya_bulko@mail.ru>
2023-05-22 23:08:26 +0300

    Initial commit

---
+++ src/main/java/anyabulko/autocheckofworks/AutocheckOfWorksApplication.java
@@ -0,0 +1,13 @@
+package anyabulko.autocheckofworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocheckOfWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocheckOfWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/anyabulko/autocheckofworks/AutocheckOfWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package anyabulko.autocheckofworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocheckOfWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
