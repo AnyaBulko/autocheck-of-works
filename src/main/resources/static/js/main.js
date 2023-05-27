3667231df5807c9cdb1639a751e3190feeda123e
Anya <anya_bulko@mail.ru>
2023-05-28 01:05:06 +0300

    Добавление Vuetify, Router

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,4 +1,58 @@
 import {createApp} from 'vue'
-import App from "./pages/App.vue";
+import App from "./pages/App.vue"
 
-createApp(App).mount("#app")
+// Vuetify
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import {aliases, md} from 'vuetify/iconsets/md'
+
+// Router
+import UsersList from './components/UsersList.vue';
+import Auth from "./pages/Auth.vue";
+import Profile from "./pages/Profile.vue";
+// import ProfileEdit from "./pages/ProfileEdit.vue";
+
+import {createWebHistory, createRouter} from "vue-router"
+
+const routes = [
+    {path: '/users', component: UsersList},
+    {path: '/login', component: Auth},
+    {path: '/profile', component: Profile},
+    // {path: '/profile/edit', component: ProfileEdit},
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+        themes: {
+            light: {
+                dark: false,
+                colors: {
+                    primary: '#3f51b5',
+                    // secondary: '#fce4ec', // #FFCDD2
+
+                }
+            },
+        },
+    },
+    icons: {
+        defaultSet: 'md',
+        aliases,
+        sets: {
+            md,
+        }
+    }
+})
+
+createApp(App).use(router).use(vuetify).mount('#app')
