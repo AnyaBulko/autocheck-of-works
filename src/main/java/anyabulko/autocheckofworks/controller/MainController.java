26db9cf74ef404146b399ada5658ad1e9bf8641f
Anya <anya_bulko@mail.ru>
2023-05-28 01:09:32 +0300

    Отправка profile на фронтенд

--- src/main/java/anyabulko/autocheckofworks/controller/MainController.java
+++ src/main/java/anyabulko/autocheckofworks/controller/MainController.java
@@ -1,6 +1,7 @@
 package anyabulko.autocheckofworks.controller;
 
-import anyabulko.autocheckofworks.repo.UserRepo;
+import anyabulko.autocheckofworks.domain.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,15 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class MainController {
-    private final UserRepo userRepo;
-
-    public MainController(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
-
     @GetMapping
-    public String main(Model model) {
-        model.addAttribute("users",userRepo.findAll());
+    public String main(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("profile",user);
         return "index";
     }
 }
